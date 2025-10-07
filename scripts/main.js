2917239ecbea412ac099b64a7b9fae62ff3d8f4a
Mortymist <mortymist@gmail.com>
2025-10-08 11:21:38 +1200

    feat: some v1

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,12 @@
-const { BackButton, requestContact, close } = window.WebApp;
+const { BackButton } = window.WebApp;
+BackButton.visible = true;
+console.log(BackButton);
 
 const mainButton = document.querySelector('#main-button');
+const textElement = document.querySelector('.mma-promo-description > p');
 
 const mainButtonClickCallback = (e) => {
-    close();
+    textElement.style.color = 'rgba(100, 50, 0, 1)';
 };
 
 mainButton.addEventListener('click', mainButtonClickCallback);
