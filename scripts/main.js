8dc9852eecdd14ef3684e55dcba59d007593bcbb
Mortymist <mortymist@gmail.com>
2025-10-08 10:50:58 +1200

    feat: добавляет начальную логику мини-приложения

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,3 @@
-document.querySelector('.mma-promo-button > button').addEventListener('click', () => {
-    window.WebApp.openMaxLink('https://mortymist.github.io/supportsvoi.github.io/');
-});
+const { BackButton } = window.WebApp;
+
+BackButton.isVisible = false;
