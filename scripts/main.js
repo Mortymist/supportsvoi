1982dbe0a6091a3150998aad0050d42af014980b
Mortymist <mortymist@gmail.com>
2025-10-08 13:39:48 +1200

    feat: some v6

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,17 @@
 const { BackButton, platform } = window.WebApp;
 BackButton.visible = true;
 console.log(window.WebApp);
-await WebApp.ScreenCapture.disableScreenCapture();
-await WebApp.requestScreenMaxBrightness();
+
 
 
 const mainButton = document.querySelector('#main-button');
 const textElement = document.querySelector('.mma-promo-description > p');
 
-const mainButtonClickCallback = (e) => {
+const mainButtonClickCallback = async (e) => {
     textElement.style.color = 'rgba(100, 50, 0, 1)';
     textElement.textContent = platform;
+    await WebApp.ScreenCapture.disableScreenCapture();
+    await WebApp.requestScreenMaxBrightness();
     window.WebApp.requestContact();
 };
 
